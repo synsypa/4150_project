220f62cbf5ad168d780f1569d82b01880c1844b4
synsypa <changjiang23@gmail.com>
2015-12-13 17:28:23 -0500

    Base Requirements 12/13/15

--- server.R
+++ server.R
@@ -1,7 +1,7 @@
 # App Implementation - SERVER
-# Superimpose Normal Dist on Histograms
+# 4150 Project
 # Construct confidence intervals
-# Last Modified: 11/21/15 by kjiang
+# Last Modified: 12/13/15 by kjiang
 
 # Initialization
 library(shiny)
@@ -17,31 +17,135 @@ priceFiles <- do.call("cbind", lapply(files, read.csv))
   names(closePrices) <- c(strtrim(files, nchar(files)-4), "date")
   closePrices <- closePrices[rev(order(as.Date(closePrices$date, format="%d-%m-%y"))),]
 
-# Draw a Histogram
+  # Create dataframe of Log-Returns for each stock
+  btu = diff(log(closePrices$btu))
+  cvx = diff(log(closePrices$cvx))
+  ed = diff(log(closePrices$ed))
+  eog = diff(log(closePrices$eog))
+  line = diff(log(closePrices$line))
+  mro = diff(log(closePrices$mro))
+  plug = diff(log(closePrices$plug))
+  regi = diff(log(closePrices$regi))
+  scty = diff(log(closePrices$scty))
+  xom = diff(log(closePrices$xom))
+  
+  log_return  = data.frame(btu,cvx,ed,eog,line,mro,plug,regi,scty,xom)
+  
 shinyServer(function(input, output) {
   
   # Generate the Histogram
   output$histPlot = renderPlot({
     stock = tolower(input$stk)
-    lPrice = diff(log(closePrices[[stock]]))
-    bins = seq(min(lPrice), max(lPrice), length.out=input$bins + 1)
+    bins = seq(min(log_return[[stock]]), max(log_return[[stock]]), length.out=input$bins + 1)
     
-    hist(lPrice, breaks = bins, prob = TRUE,
+    hist(log_return[[stock]], breaks = bins, prob = TRUE,
          main = paste("Diff of Log Returns for ", stock), xlab = "Diff Log Return")
-    curve(dnorm(x, mean = mean(lPrice), sd = sd(lPrice)), add = TRUE)
+    curve(dnorm(x, mean = mean(log_return[[stock]]), sd = sd(log_return[[stock]])), add = TRUE)
   })
   
-  # Calculate Confidence Interval
-  output$confInt = renderText({
+  # Calculate Confidence Interval of Mean
+  output$confMean = renderText({
     stock = tolower(input$stk)
-    lPrice = diff(log(closePrices[[stock]]))
-    mean = mean(lPrice)
-    sd = sd(lPrice)
-    n = length(lPrice) 
+    mean = mean(log_return[[stock]])
+    sd = sd(log_return[[stock]])
+    n = length(log_return[[stock]]) 
     error = qt(input$cLevel, df=n-1)*sd/sqrt(n)
     left = round(mean - error, digits = 5)
     right = round(mean + error, digits = 5)
     
-    paste("The ", input$cLevel, "Confidence Interval is [", left, right, "]")
+    paste("The ", input$cLevel, "Confidence Interval of the Mean is [", left,",", right, "]")
   })
+  
+  # Calculate Confidence Interval of SD
+  output$confSD <- renderText({ 
+      stock = tolower(input$stk)
+      mean = mean(log_return[[stock]])
+      sd = sd(log_return[[stock]])
+      n = length(log_return[[stock]]) 
+      error = sqrt(((n-1)*sd^2)/qchisq(input$cLevel, df=n-1 ))
+      left <-round(sd - error, digits = 5)
+      right <- round(sd + error, digits = 5)
+      
+      paste("The ", input$cLevel, "Confidence interval of the standard deviation is [", left,",", right,"]")
+    })
+  
+  # Produce Regression Plot for Stock vs Time
+  output$s1_regPlot <- renderPlot({
+    stock = tolower(input$stk2)
+    lr = log_return[[stock]]
+    n = length(lr) 
+    time <- seq(from=1,to = n, by = 1)
+    model <- lm(lr ~ time)
+    plot(time,lr, main = "Log Returns w/ fitted regression", xlab="Trading days since beginning 2014",ylab = input$stk2)
+    abline(model)
+    
+    output$s1_res <- renderPlot({
+      plot(resid(model),ylim =c(min(resid(model)), max(resid(model))) ,main = "Graph of residuals",xlab = "Trading days since beginning 2014",ylab = "Residuals" ) 
+    }) 
+  })
+  
+  # Produce Regression Results for Stock vs Time
+  output$s1_regRes <- renderText({ 
+    stock = tolower(input$stk2)
+    lr = log_return[[stock]]
+    n <- length(lr)
+    time <- seq(from=1,to = n, by = 1)
+    model <- lm(lr ~ time)
+    plot(time,lr,xlab="Trading days since beginning 2014",ylab = input$stk2)
+    abline(model)
+    paste("Slope of regression line = ",round(model$coefficients[2],digits = 5),"
+          ","Intercept = ",round(model$coefficients[1],digits =5), "R-squared = ",
+          round(summary(model)$r.squared,digits = 5))
+    })
+  
+  # Produce Regression Plot for Stock vs Stock
+  output$s2_regPlot <- renderPlot({
+    stock1 = tolower(input$stkA)
+    stock2 = tolower(input$stkB)
+    lr1 = log_return[[stock1]]
+    lr2 = log_return[[stock2]]
+    n = length(lr1)
+    model = lm(lr1 ~ lr2)
+    plot(lr1,lr2,xlab = stock2 ,ylab = stock1)
+    abline(model)
+    output$s2_res <- renderPlot({
+      plot(resid(model),ylim =c(min(resid(model)), max(resid(model))) ,main = "Graph of residuals",xlab = "",ylab = "Residuals" ) 
+    }) 
+  })
+  
+  # Produce Regression Results for Stock vs Stock
+  output$s2_regRes <- renderText({ 
+    stock1 = tolower(input$stkA)
+    stock2 = tolower(input$stkB)
+    lr1 = log_return[[stock1]]
+    lr2 = log_return[[stock2]]
+    n = length(lr1)
+    model <- lm(lr1 ~ lr2)
+    paste("Slope of regression line = ",round(model$coefficients[2],digits = 5),"
+            ","Intercept = ",round(model$coefficients[1],digits =5),"R-Squared = ",
+            round(summary(model)$r.squared,digits = 5))
+    })
+  
+  # Produce t-test results
+  output$s2_ttest <- renderText({
+      if (input$stkC != input$stkD){
+        stock1 = tolower(input$stkC)
+        stock2 = tolower(input$stkD)
+        lr1 = log_return[[stock1]]
+        lr2 = log_return[[stock2]]
+        test = t.test(lr1,lr2,paired=TRUE)
+        if (test$p.value > input$cLevel2) {
+          paste("Under a paired t-test, we cannot reject the hypothesis that the mean log-returns of ", toupper(stock1), " and ", toupper(stock2), " are equal. (p-value = ", round(test$p.value, digits = 5), ") at Confidence Level = ", input$cLevel2)
+        }
+        else {
+          paste("Under a paired t-test, we REJECT the hypothesis that the mean log-returns of ", toupper(stock1), " and ", toupper(stock2), " are equal (p-value = ", round(test$p.value, digits = 5), ") at Confidence Level = ", input$cLevel2)
+        }
+        
+      }
+      else(paste("You have selected the same stock, please select 2 different stocks"))      
+      
+      
+      
+    })
+  
 })
--- ui.R
+++ ui.R
@@ -1,41 +1,140 @@
 # App Implementation - UI
-# Superimpose Normal Dist on Histograms
-# Construct confidence intervals
-# Last Modified: 11/21/15 by kjiang
+# 4150 Project
+# Last Modified: 12/13/15 by kjiang
 
 # Initialization
 library(shiny)
 
-# Define UI to draw Histogram
-shinyUI(fluidPage(
-  
-# Application Title
-  titlePanel("4150 Group HW 3"),
+# Navigation Bar
+shinyUI(navbarPage("Energy Stock Analysis",
+                   tabPanel("Single Stock"),
+                   tabPanel("Two Stock")
+))
 
-# Choosing stock to show
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("stk", "Choose Stock (by symbol):", 
-                  c("Peabody Energy Corp"="btu", "Chevron Corp"="cvx",
-                    "Consolidated Edison, Inc"="ed", "EOG Resources Inc"="eog",
-                    "Linn Energy LLC"="line", "Marathon Oil Corp"="mro",
-                    "Plug Power Inc."="plug", "Renewable Energy Group, Inc"="regi",
-                    "SolarCity Corp"="scty", "Exxon Mobil Corp"="xom")),
+# Define UI for Single Stock analysis
+shinyUI(
+  fluidPage(
+    navbarPage("Energy Stock Analysis",
       
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1, max = 100, value = 50, step = 1, ticks = FALSE),
-      
-      sliderInput("cLevel",
-                  "Confidence Level:",
-                  min = 0, max = 1, value = .95, step = .01)
-      
-  ),
+              # Single Stock Base Analysis
+                tabPanel("Histograms and CIs",
+                  titlePanel("Basic Statistics"),
+                  # Inputs for Single Stock Analysis
+                  sidebarLayout(
+                    sidebarPanel(
+                      # Select Stock to View
+                      selectInput("stk", "Choose Stock:", 
+                                  c("Peabody Energy Corp"="btu", "Chevron Corp"="cvx",
+                                    "Consolidated Edison, Inc"="ed", "EOG Resources Inc"="eog",
+                                    "Linn Energy LLC"="line", "Marathon Oil Corp"="mro",
+                                    "Plug Power Inc."="plug", "Renewable Energy Group, Inc"="regi",
+                                    "SolarCity Corp"="scty", "Exxon Mobil Corp"="xom")),
+                      
+                      # Select bins for histogram      
+                      sliderInput("bins",
+                                  "Number of bins:",
+                                  min = 1, max = 100, value = 50, step = 1, ticks = FALSE),
+                      
+                      # Select Confidence Level
+                      sliderInput("cLevel",
+                                  "Confidence Level:",
+                                  min = 0, max = 1, value = .95, step = .01)
+                    ),
 
-# Display Histogram and Confidence Interval
-  mainPanel(
-    plotOutput("histPlot"),
-    textOutput("confInt")
+                    # Display Histogram and Confidence Interval
+                    mainPanel(
+                      plotOutput("histPlot", width = "100%"),
+                      textOutput("confMean"),
+                      textOutput("confSD")
+                    )
+                    )
+                ),
+              
+              # Single Stock Regression
+              navbarMenu("Single Stock Analysis",
+                tabPanel("Linear Regressions",
+                  titlePanel("Single-stock Regressions on Time"),
+                  sidebarLayout(
+                    sidebarPanel(
+                      # Select Stock to View
+                      selectInput("stk2", "Choose Stock:", 
+                                  c("Peabody Energy Corp"="btu", "Chevron Corp"="cvx",
+                                    "Consolidated Edison, Inc"="ed", "EOG Resources Inc"="eog",
+                                    "Linn Energy LLC"="line", "Marathon Oil Corp"="mro",
+                                    "Plug Power Inc."="plug", "Renewable Energy Group, Inc"="regi",
+                                    "SolarCity Corp"="scty", "Exxon Mobil Corp"="xom"))
+                    ),
+                    
+                    # Display Regression Output          
+                    mainPanel(
+                      plotOutput("s1_regPlot"),
+                      textOutput("s1_regRes"),
+                      plotOutput("s1_res")
+                    )
+                  )
+                )
+              ),
+              
+              # Multi Stock Analysis
+              navbarMenu("Two Stock Analysis",                     
+                         tabPanel("t-testing",
+                                  titlePanel("Two-stock Returns Comparison"),
+                                  sidebarLayout(
+                                    sidebarPanel(
+                                      # Select Stocks to Compare (C vs D)
+                                      selectInput("stkC", "Choose Stock:", 
+                                                  c("Peabody Energy Corp"="btu", "Chevron Corp"="cvx",
+                                                    "Consolidated Edison, Inc"="ed", "EOG Resources Inc"="eog",
+                                                    "Linn Energy LLC"="line", "Marathon Oil Corp"="mro",
+                                                    "Plug Power Inc."="plug", "Renewable Energy Group, Inc"="regi",
+                                                    "SolarCity Corp"="scty", "Exxon Mobil Corp"="xom")),
+                                      
+                                      selectInput("stkD", "Choose Stock:", 
+                                                  c("Peabody Energy Corp"="btu", "Chevron Corp"="cvx",
+                                                    "Consolidated Edison, Inc"="ed", "EOG Resources Inc"="eog",
+                                                    "Linn Energy LLC"="line", "Marathon Oil Corp"="mro",
+                                                    "Plug Power Inc."="plug", "Renewable Energy Group, Inc"="regi",
+                                                    "SolarCity Corp"="scty", "Exxon Mobil Corp"="xom")),
+                                      
+                                      # Select Confidence Level
+                                      sliderInput("cLevel2",
+                                                  "Confidence Level:",
+                                                  min = 0, max = 1, value = .95, step = .01)
+                                    ),
+                                    mainPanel(
+                                      textOutput("s2_ttest")
+                                    )
+                                  )
+                         ),
+                         tabPanel("Linear Regressions",
+                                  titlePanel("Two-stock Regressions"),
+                                  sidebarLayout(
+                                    sidebarPanel(
+                                      # Select Stocks to Compare (A vs B)
+                                      selectInput("stkA", "Choose Stock:", 
+                                                  c("Peabody Energy Corp"="btu", "Chevron Corp"="cvx",
+                                                    "Consolidated Edison, Inc"="ed", "EOG Resources Inc"="eog",
+                                                    "Linn Energy LLC"="line", "Marathon Oil Corp"="mro",
+                                                    "Plug Power Inc."="plug", "Renewable Energy Group, Inc"="regi",
+                                                    "SolarCity Corp"="scty", "Exxon Mobil Corp"="xom")),
+                                      
+                                      selectInput("stkB", "Choose Stock:", 
+                                                  c("Peabody Energy Corp"="btu", "Chevron Corp"="cvx",
+                                                    "Consolidated Edison, Inc"="ed", "EOG Resources Inc"="eog",
+                                                    "Linn Energy LLC"="line", "Marathon Oil Corp"="mro",
+                                                    "Plug Power Inc."="plug", "Renewable Energy Group, Inc"="regi",
+                                                    "SolarCity Corp"="scty", "Exxon Mobil Corp"="xom"))
+                                    ),
+                                    
+                                    # Display Regression Output          
+                                    mainPanel(
+                                      plotOutput("s2_regPlot"),
+                                      textOutput("s2_regRes"),
+                                      plotOutput("s2_res")
+                                    )
+                                  )
+                         )
+              )
+    )
   )
 )
-))
